2eb3f6a601eb5980defdf5a533c226f12fc8e715
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-18 04:50:01 +0500

    New CodeWars challenges added

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -5,5 +5,6 @@ public class EvenOrOdd {
     }
     public static void main(String[] args) {
         System.out.println(evenOrOdd(5));
+        System.out.println(evenOrOdd(4));
     }
 }
---
+++ src/StringCleaning.java
@@ -0,0 +1,9 @@
+public class StringCleaning {
+    static String stringClean(final String text) {
+       return text.replaceAll("\\d", "");
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(stringClean("Artorias9 has one broken 1arm!"));
+    }
+}
