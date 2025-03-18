2dc255696e634e3a60c4a80c50795b9ca3c46684
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-18 20:49:54 +0500

    Update StringCleaning.java

--- src/StringCleaning.java
+++ src/StringCleaning.java
@@ -5,5 +5,6 @@ public class StringCleaning {
     }
     public static void main(String[] args) {
         System.out.println(stringClean("Artorias9 has one broken 1arm!"));
+        System.out.println(stringClean("123Dark 0Souls!"));
     }
 }
