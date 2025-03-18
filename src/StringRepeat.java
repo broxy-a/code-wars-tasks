201a13339ad3cb4840834a72e1ec5a6cd81d63ff
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-18 20:51:37 +0500

    Update StringRepeat.java

--- src/StringRepeat.java
+++ src/StringRepeat.java
@@ -4,5 +4,7 @@ public class StringRepeat {
     }
 public static void main(String[] args) {
         System.out.println(repeatStr(5, "Artorias"));
+        System.out.println(repeatStr(5, "Souls"));
+        System.out.println(repeatStr(5, "Knight"));
     }
 }
