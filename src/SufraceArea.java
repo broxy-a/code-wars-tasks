c6f3f7598817b22e046c2d897b9f75f48f68b540
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-21 21:35:15 +0500

    Update SufraceArea.java

--- src/SufraceArea.java
+++ src/SufraceArea.java
@@ -8,5 +8,7 @@ public class SufraceArea {
     }
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getSize(5, 15, 10)));
+        System.out.println(Arrays.toString(getSize(4, 2, 6)));
+        System.out.println(Arrays.toString(getSize(5, 5, 5)));
     }
 }
