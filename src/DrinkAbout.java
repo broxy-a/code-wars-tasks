6d078c9132d53aa657ff254ed9834429844e7d47
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-21 20:33:54 +0500

    new codewars task

--- src/DrinkAbout.java
+++ src/DrinkAbout.java
@@ -1,21 +1,14 @@
 public class DrinkAbout {
     public static String peopleWithAgeDrink(int age) {
-        if (age < 14) {
-            return "drink toddy";
-        } else if (age < 18) {
-            return "drink coke";
-        } else if (age < 21) {
-            return "drink beer";
-        } else if  (age >= 21) {
-            return "drink whisky";
-        }
-        return "";
+        return age < 14 ? "drink toddy" : age < 18 ? "drink coke" : age < 21 ? "drink beer" : (age >= 21 ? "drink whisky" : "");
     }
     public static void main(String[] args) {
     System.out.println(peopleWithAgeDrink(13));  
     System.out.println(peopleWithAgeDrink(17));  
+    System.out.println(peopleWithAgeDrink(13)); 
+    System.out.println(peopleWithAgeDrink(17)); 
     System.out.println(peopleWithAgeDrink(19));  
-    System.out.println(peopleWithAgeDrink(21));  
+    System.out.println(peopleWithAgeDrink(21));
 
     }   
 }
---
+++ src/SufraceArea.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SufraceArea {
+    public static int[] getSize(int w, int h, int d) {
+        int S = 2 * (w * h + w * d + h * d);
+        int V = w * h * d;
+        return new int[]{S, V};
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getSize(5, 15, 10)));
+    }
+}
