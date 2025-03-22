ccb011e70bd70af9c6026f67b77b11fedf7aecd5
broxy-a <abdrahmanovamir2001@gmail.com>
2025-03-22 20:21:41 +0500

    New codewars task

---
+++ src/HowGoodAreYou.java
@@ -0,0 +1,16 @@
+public class HowGoodAreYou {
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        int sum = 0;
+        for (int i = 0; i < classPoints.length; i++) {
+            sum = sum + classPoints[i];
+        }
+        int GPA = sum / classPoints.length;
+        return yourPoints > GPA ? true : false; 
+
+    }
+    public static void main(String[] args) {
+        System.out.println(HowGoodAreYou.betterThanAverage(new int[]{2, 3}, 5));    
+        System.out.println(HowGoodAreYou.betterThanAverage(new int[]{2, 3}, 1));    
+    }
+
+}
